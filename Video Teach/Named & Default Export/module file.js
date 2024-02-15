271c6aaebf8522342f7c91d9152f701f3db33429
Debjeet Ghosh <debjeetghosh2149@gmail.com>
2024-02-15 23:06:47 +0530

    Create module file.js

---
+++ Video Teach/Named & Default Export/module file.js
@@ -0,0 +1,19 @@
+// one way to named export it
+export const a = 2
+export const b = 21
+
+
+// one way to export by default
+export default obj = {
+    a : 5,
+    y : 23
+}
+
+//  we can do default export in another way
+
+// const obj = {
+//     a : 5,
+//     y : 23
+// }
+
+// export default obj;
